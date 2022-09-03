3c50dc9fbed0fe230eee03bde9169c82d8e0f11e
João Lenon <lenonSec7@gmail.com>
2022-09-03 00:24:29 -0300

    fix(config): set env default as local

--- config/app.js
+++ config/app.js
@@ -14,7 +14,7 @@ export default {
   |
   */
 
-  environment: process.env.NODE_ENV || 'production',
+  environment: process.env.NODE_ENV || 'local',
 
   /*
   |--------------------------------------------------------------------------
